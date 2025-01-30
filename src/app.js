8ce8deb389c045d67e8e4cd7482bc1ed7e56a583
AbhayBansal <126644386+Targter@users.noreply.github.com>
2025-01-30 13:57:48 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -2,8 +2,18 @@ import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 
-const app = express();
-app.use(cors({ origin: process.env.CORS_ORIGIN, credentials: true }));
+const app = express();app.use(cors({
+  origin: function (origin, callback) {
+    if (origin && origin !== process.env.CORS_ORIGIN) {
+      console.log('Blocked CORS origin: ', origin);  // Log blocked origin
+      callback(new Error('CORS Not Allowed'), false);
+    } else {
+      callback(null, true);
+    }
+  },
+  credentials: true
+}));
+
 app.use(express.json({ limit: "40kb" }));
 app.use(express.urlencoded({ extended: true, limit: "20kb" }));
 app.use(express.static("public"));
